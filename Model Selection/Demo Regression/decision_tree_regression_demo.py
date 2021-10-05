a966a19b4862ec3c53ffeb82570e1641b0f569eb
momchilantonov <eng.antonov@gmail.com>
2021-10-06 02:14:52 +0300

    Resources upload and Model Selection Demo - Python

---
+++ Model Selection/Demo Regression/decision_tree_regression_demo.py
@@ -0,0 +1,27 @@
+# imports
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+
+# read data
+data = pd.read_csv('Data.csv')
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+# split the data into the training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+# create and train the model
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X_train, y_train)
+
+# predict the test set results
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# evaluate the model performance
+r2_score(y_test, y_pred)
